d4ca6dd2ec8669d19ab5b01def9f38f037d13714
Zarun Ch <zarun69@gmail.com>
2017-12-07 01:15:06 +0700

    แก้ดีไซน์หน้าประเภทสินค้า

--- CourtBooking/POS/ProductsInfo.Designer.cs
+++ CourtBooking/POS/ProductsInfo.Designer.cs
@@ -32,203 +32,124 @@
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.textBox2 = new DevExpress.XtraEditors.TextEdit();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.checkEdit8 = new DevExpress.XtraEditors.CheckEdit();
-            this.checkEdit9 = new DevExpress.XtraEditors.CheckEdit();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.checkEdit4 = new DevExpress.XtraEditors.CheckEdit();
-            this.checkEdit5 = new DevExpress.XtraEditors.CheckEdit();
-            this.checkEdit6 = new DevExpress.XtraEditors.CheckEdit();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.checkEdit3 = new DevExpress.XtraEditors.CheckEdit();
-            this.checkEdit2 = new DevExpress.XtraEditors.CheckEdit();
-            this.checkEdit1 = new DevExpress.XtraEditors.CheckEdit();
-            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
-            this.groupControl1.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit8.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit9.Properties)).BeginInit();
-            this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit4.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit5.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit6.Properties)).BeginInit();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit3.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit2.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).BeginInit();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.dropDownButton1 = new DevExpress.XtraEditors.DropDownButton();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.textBox1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBox2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(123, 52);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(173, 24);
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
             this.textBox1.TabIndex = 132;
             // 
             // labelControl1
             // 
             this.labelControl1.Location = new System.Drawing.Point(12, 57);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(72, 13);
+            this.labelControl1.Size = new System.Drawing.Size(45, 13);
             this.labelControl1.TabIndex = 130;
-            this.labelControl1.Text = "รหัส Permission";
+            this.labelControl1.Text = "รหัสสินค้า";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(123, 82);
+            this.textBox2.Location = new System.Drawing.Point(123, 78);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(352, 24);
+            this.textBox2.Size = new System.Drawing.Size(481, 20);
             this.textBox2.TabIndex = 133;
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(12, 87);
+            this.labelControl2.Location = new System.Drawing.Point(12, 83);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(67, 13);
+            this.labelControl2.Size = new System.Drawing.Size(40, 13);
             this.labelControl2.TabIndex = 131;
-            this.labelControl2.Text = "ชือ Permission";
+            this.labelControl2.Text = "ชือสินค้า";
             // 
-            // groupControl1
+            // textEdit1
             // 
-            this.groupControl1.Controls.Add(this.groupBox3);
-            this.groupControl1.Controls.Add(this.groupBox2);
-            this.groupControl1.Controls.Add(this.groupBox1);
-            this.groupControl1.Location = new System.Drawing.Point(12, 112);
-            this.groupControl1.Name = "groupControl1";
-            this.groupControl1.Size = new System.Drawing.Size(548, 257);
-            this.groupControl1.TabIndex = 134;
-            this.groupControl1.Text = "ข้อมูลที่สามารถเข้าใช้งานได้";
+            this.textEdit1.Location = new System.Drawing.Point(123, 133);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(173, 20);
+            this.textEdit1.TabIndex = 135;
             // 
-            // groupBox3
+            // labelControl3
             // 
-            this.groupBox3.Controls.Add(this.checkEdit8);
-            this.groupBox3.Controls.Add(this.checkEdit9);
-            this.groupBox3.Location = new System.Drawing.Point(364, 30);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(169, 107);
-            this.groupBox3.TabIndex = 4;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Administrator";
+            this.labelControl3.Location = new System.Drawing.Point(12, 136);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(51, 13);
+            this.labelControl3.TabIndex = 134;
+            this.labelControl3.Text = "ราคาต้นทุน";
             // 
-            // checkEdit8
+            // dropDownButton1
             // 
-            this.checkEdit8.Location = new System.Drawing.Point(6, 45);
-            this.checkEdit8.Name = "checkEdit8";
-            this.checkEdit8.Properties.Caption = "User";
-            this.checkEdit8.Size = new System.Drawing.Size(119, 19);
-            this.checkEdit8.TabIndex = 1;
+            this.dropDownButton1.Location = new System.Drawing.Point(123, 104);
+            this.dropDownButton1.Name = "dropDownButton1";
+            this.dropDownButton1.Size = new System.Drawing.Size(173, 23);
+            this.dropDownButton1.TabIndex = 136;
             // 
-            // checkEdit9
+            // textEdit2
             // 
-            this.checkEdit9.Location = new System.Drawing.Point(6, 20);
-            this.checkEdit9.Name = "checkEdit9";
-            this.checkEdit9.Properties.Caption = "Permission";
-            this.checkEdit9.Size = new System.Drawing.Size(119, 19);
-            this.checkEdit9.TabIndex = 0;
+            this.textEdit2.Location = new System.Drawing.Point(431, 133);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(173, 20);
+            this.textEdit2.TabIndex = 138;
             // 
-            // groupBox2
+            // labelControl4
             // 
-            this.groupBox2.Controls.Add(this.checkEdit4);
-            this.groupBox2.Controls.Add(this.checkEdit5);
-            this.groupBox2.Controls.Add(this.checkEdit6);
-            this.groupBox2.Location = new System.Drawing.Point(189, 30);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(169, 107);
-            this.groupBox2.TabIndex = 3;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "POS";
+            this.labelControl4.Location = new System.Drawing.Point(320, 136);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(40, 13);
+            this.labelControl4.TabIndex = 137;
+            this.labelControl4.Text = "ราคาขาย";
             // 
-            // checkEdit4
+            // labelControl5
             // 
-            this.checkEdit4.Location = new System.Drawing.Point(6, 70);
-            this.checkEdit4.Name = "checkEdit4";
-            this.checkEdit4.Properties.Caption = "การขาย";
-            this.checkEdit4.Size = new System.Drawing.Size(119, 19);
-            this.checkEdit4.TabIndex = 2;
+            this.labelControl5.Location = new System.Drawing.Point(12, 109);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(62, 13);
+            this.labelControl5.TabIndex = 139;
+            this.labelControl5.Text = "ประเภทสินค้า";
             // 
-            // checkEdit5
-            // 
-            this.checkEdit5.Location = new System.Drawing.Point(6, 45);
-            this.checkEdit5.Name = "checkEdit5";
-            this.checkEdit5.Properties.Caption = "สินค้า";
-            this.checkEdit5.Size = new System.Drawing.Size(119, 19);
-            this.checkEdit5.TabIndex = 1;
-            // 
-            // checkEdit6
-            // 
-            this.checkEdit6.Location = new System.Drawing.Point(6, 20);
-            this.checkEdit6.Name = "checkEdit6";
-            this.checkEdit6.Properties.Caption = "หน่วยนับ";
-            this.checkEdit6.Size = new System.Drawing.Size(119, 19);
-            this.checkEdit6.TabIndex = 0;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.checkEdit3);
-            this.groupBox1.Controls.Add(this.checkEdit2);
-            this.groupBox1.Controls.Add(this.checkEdit1);
-            this.groupBox1.Location = new System.Drawing.Point(14, 30);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(169, 107);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Court  Booking";
-            // 
-            // checkEdit3
-            // 
-            this.checkEdit3.Location = new System.Drawing.Point(6, 70);
-            this.checkEdit3.Name = "checkEdit3";
-            this.checkEdit3.Properties.Caption = "ข้อมูลลูกค้า";
-            this.checkEdit3.Size = new System.Drawing.Size(119, 19);
-            this.checkEdit3.TabIndex = 2;
-            // 
-            // checkEdit2
-            // 
-            this.checkEdit2.Location = new System.Drawing.Point(6, 45);
-            this.checkEdit2.Name = "checkEdit2";
-            this.checkEdit2.Properties.Caption = "ข้อมูลการจองสนาม";
-            this.checkEdit2.Size = new System.Drawing.Size(119, 19);
-            this.checkEdit2.TabIndex = 1;
-            // 
-            // checkEdit1
-            // 
-            this.checkEdit1.Location = new System.Drawing.Point(6, 20);
-            this.checkEdit1.Name = "checkEdit1";
-            this.checkEdit1.Properties.Caption = "ข้อมูล Promotion";
-            this.checkEdit1.Size = new System.Drawing.Size(119, 19);
-            this.checkEdit1.TabIndex = 0;
-            // 
-            // PermissionInfo
+            // ProductsInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(573, 398);
-            this.Controls.Add(this.groupControl1);
+            this.ClientSize = new System.Drawing.Size(616, 398);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.dropDownButton1);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.labelControl3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.labelControl2);
-            this.Name = "PermissionInfo";
+            this.Name = "ProductsInfo";
             this.Text = "สินค้า";
             this.Controls.SetChildIndex(this.labelControl2, 0);
             this.Controls.SetChildIndex(this.textBox2, 0);
             this.Controls.SetChildIndex(this.labelControl1, 0);
             this.Controls.SetChildIndex(this.textBox1, 0);
-            this.Controls.SetChildIndex(this.groupControl1, 0);
-            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
-            this.groupControl1.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit8.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit9.Properties)).EndInit();
-            this.groupBox2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit4.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit5.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit6.Properties)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit3.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit2.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).EndInit();
+            this.Controls.SetChildIndex(this.labelControl3, 0);
+            this.Controls.SetChildIndex(this.textEdit1, 0);
+            this.Controls.SetChildIndex(this.dropDownButton1, 0);
+            this.Controls.SetChildIndex(this.labelControl4, 0);
+            this.Controls.SetChildIndex(this.textEdit2, 0);
+            this.Controls.SetChildIndex(this.labelControl5, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.textBox1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBox2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -240,17 +161,11 @@
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.TextEdit textBox2;
         private DevExpress.XtraEditors.LabelControl labelControl2;
-        private DevExpress.XtraEditors.GroupControl groupControl1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private DevExpress.XtraEditors.CheckEdit checkEdit2;
-        private DevExpress.XtraEditors.CheckEdit checkEdit1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private DevExpress.XtraEditors.CheckEdit checkEdit4;
-        private DevExpress.XtraEditors.CheckEdit checkEdit5;
-        private DevExpress.XtraEditors.CheckEdit checkEdit6;
-        private DevExpress.XtraEditors.CheckEdit checkEdit3;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private DevExpress.XtraEditors.CheckEdit checkEdit8;
-        private DevExpress.XtraEditors.CheckEdit checkEdit9;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.DropDownButton dropDownButton1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
     }
 }
